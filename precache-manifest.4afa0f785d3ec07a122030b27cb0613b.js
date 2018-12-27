09d3cfb4cb64a0b456bfd0dd88c645d9ee735db6
skudryavtsev <s2.kudryavtsev@itransition.com>
2018-12-27 20:43:32 +0300

    publish #3

---
+++ precache-manifest.4afa0f785d3ec07a122030b27cb0613b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "50676f2728b0c9f34336",
+    "url": "/wizzard-react-typescript/static/js/runtime~main.50676f27.js"
+  },
+  {
+    "revision": "075c267ca844ee80f24e",
+    "url": "/wizzard-react-typescript/static/js/main.075c267c.chunk.js"
+  },
+  {
+    "revision": "f5be82589ff580063075",
+    "url": "/wizzard-react-typescript/static/js/1.f5be8258.chunk.js"
+  },
+  {
+    "revision": "075c267ca844ee80f24e",
+    "url": "/wizzard-react-typescript/static/css/main.7553a321.chunk.css"
+  },
+  {
+    "revision": "198de4b57cbb25d87fd73665c10d14ae",
+    "url": "/wizzard-react-typescript/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/wizzard-react/precache-manifest.48230195792156d5fba28ca260eb283d.js"
+  "/wizzard-react-typescript/precache-manifest.4afa0f785d3ec07a122030b27cb0613b.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/wizzard-react/index.html", {
+workbox.routing.registerNavigationRoute("/wizzard-react-typescript/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
