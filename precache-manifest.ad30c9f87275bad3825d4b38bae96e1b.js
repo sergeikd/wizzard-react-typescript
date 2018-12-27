b30ee16f5aba8d0615f35ad5f5c6bd56a5c86f63
skudryavtsev <s2.kudryavtsev@itransition.com>
2018-12-27 20:30:14 +0300

    publish build

---
+++ precache-manifest.ad30c9f87275bad3825d4b38bae96e1b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9a65397143d6d924f05c",
+    "url": "/wizzard-react/static/js/runtime~main.9a653971.js"
+  },
+  {
+    "revision": "4ceac70c9bdfd2570285",
+    "url": "/wizzard-react/static/js/main.4ceac70c.chunk.js"
+  },
+  {
+    "revision": "f5be82589ff580063075",
+    "url": "/wizzard-react/static/js/1.f5be8258.chunk.js"
+  },
+  {
+    "revision": "4ceac70c9bdfd2570285",
+    "url": "/wizzard-react/static/css/main.a8001c51.chunk.css"
+  },
+  {
+    "revision": "bc0edd96aa5b26f91badb58177e7acd2",
+    "url": "/wizzard-react/index.html"
+  }
+];
--- src/common/interfaces.ts
+++
@@ -1,9 +0,0 @@
-export interface IEntity {
-    id: number;
-    name: string;
-}
-
-export interface IPages {
-    title: string;
-    entity: string;
-}
